1609babd2725460b2633708fcc2c080c3c9b0afb
pengjunjie <pengjunjie@rongcapital.cn>
2019-03-18 22:16:05 +0800

    support spring jbehave.

---
+++ jbehave-demo-spring/src/test/java/io/william/jbehave/demo/basic/SpringJBehaveDemo.java
@@ -0,0 +1,56 @@
+package io.william.jbehave.demo.basic;
+
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.jbehave.core.steps.spring.SpringApplicationContextFactory;
+import org.jbehave.core.steps.spring.SpringStepsFactory;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import steps.TraderSteps;
+
+import java.util.List;
+
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=SpringJBehaveDemo.class)
+public class SpringJBehaveDemo extends JUnitStories {
+
+    @Override
+    public Configuration configuration() {
+        return new MostUsefulConfiguration()
+                .useStoryLoader(new LoadFromClasspath(this.getClass().getClassLoader()))
+                .useStoryReporterBuilder(new StoryReporterBuilder()
+                        .withDefaultFormats()
+                        //note:it's important to avoid report error.
+                        .withRelativeDirectory("jbehave-report")
+                        .withCodeLocation(codeLocationFromClass(this.getClass()))
+                        .withFormats(Format.CONSOLE, Format.TXT, Format.HTML, Format.XML));
+    }
+
+    @Override
+    protected List<String> storyPaths() {
+        return new StoryFinder().findPaths(
+                codeLocationFromClass(this.getClass()), "stories/*.story", null);
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(
+                configuration(), new TraderSteps());
+    }
+
+}
---
+++ jbehave-demo-spring/src/test/java/steps/TraderSteps.java
@@ -0,0 +1,31 @@
+package steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.junit.Assert;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TraderSteps {
+
+	private double threshold;
+	private double price;
+
+	@Given("a stock of symbol $symbol and a threshold of $threshold")
+	public void aStock(String symbol, double threshold) {
+		this.threshold = threshold;
+	}
+
+	@When("the stock is traded at $price")
+	public void theStockIsTradedAt(double price) {
+		this.price = price;
+	}
+
+	@Then("the alert status should be $status")
+	public void theAlertStatusShouldBe(String status) {
+		Assert.assertTrue((price > threshold) ? "ON".equals(status) : "OFF".equals(status));
+	}
+
+}
